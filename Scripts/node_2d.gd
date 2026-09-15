319d0860128b069a18f2b5c2bd7887089e7a9e38
Kristopher Bendrick <kristopherbendrick@KristoprsMBP379.attlocal.net>
2026-09-15 02:06:26 -0500

    Pathing and Map Experiment

---
+++ Scripts/node_2d.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var map: TileMapLayer
+var astar_grid: AStarGrid2D
+
+func _ready():
+	astar_grid = AStarGrid2D.new()
+	astar_grid.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar_grid.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar_grid.cell_size = map.tile_set.tile_size
+	astar_grid.region = Rect2(Vector2.ZERO, ceil(get_viewport_rect().size / astar_grid.cell_size))
+	astar_grid.update()
+	
+	for id in map.get_used_cells():
+		var data = map.get_cell_tile_data(id)
+		if data and data.get_custom_data('obstacle'):
+			astar_grid.set_point_solid(id)
+
+	%GridDisplay.grid = astar_grid
+	%Player.setup(astar_grid)
---
+++ Scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var grid: AStarGrid2D
+
+var current_cell: Vector2i
+var target_cell: Vector2i
+var move_pts: Array
+
+func setup(_grid: AStarGrid2D):
+	grid = _grid
+	current_cell = pos_to_cell(global_position)
+	target_cell = current_cell
+
+func pos_to_cell(pos: Vector2):
+	return pos / grid.cell_size
+	
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		var target = Vector2i(pos_to_cell(event.position))
+		if target != target_cell:
+			move_pts = grid.get_point_path(current_cell, target)
+			move_pts = (move_pts as Array).map(func (p): return p + grid.cell_size / 2.0)
+			$PathPreviz.points = move_pts
+			target_cell = target
