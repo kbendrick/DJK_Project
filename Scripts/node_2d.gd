b27df3c2d130fbd742d1eeb40a24be565d8a73b1
protologos <jbtcomposer@gmail.com>
2026-09-15 21:46:32 -0400

    Added RNG and set_shelves function

--- Scripts/node_2d.gd
+++ Scripts/node_2d.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var map: TileMapLayer
 var astar_grid: AStarGrid2D
 
+# Random number generation. Could use some sort of seed implementation to check on consistent results?
+var rng = RandomNumberGenerator.new()
+
 func _ready():
 	astar_grid = AStarGrid2D.new()
 	astar_grid.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
@@ -19,3 +22,16 @@ func _ready():
 
 	%GridDisplay.grid = astar_grid
 	%Player.setup(astar_grid)
+
+func set_shelves(marker_count): 
+	# Introduces five possible states of shelves with relative weights below, then initializes array for shelf states
+	var possible_states = ["very bad", "bad", "neutral", "good", "very good"]
+	var state_weights = PackedFloat32Array([0.25, 0.5, 1, 0.5, 0.25])
+	var shelf_states = []
+
+	# For loops to append a shelf state for every marker in a room with rand_weighted
+	for marker in marker_count:
+		shelf_states.append(possible_states[rng.rand_weighted(state_weights)])
+
+	return shelf_states
+	
